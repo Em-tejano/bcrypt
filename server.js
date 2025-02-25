0a794e95de3f92651c993340cd88a0f842b5d389
Em-tejano <mtejano.mt@gmail.com>
2025-02-25 12:04:49 +0800

    Async hash generate and compare try 1

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    //storing hash
+    bcrypt.compare(myPlaintextPassword, hash, (err, hash) => {
+        //res = true/false
+    })
+});
 
 
 //END_ASYNC
