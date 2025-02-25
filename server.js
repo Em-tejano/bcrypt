fefcce278794a51d74cc5f1812d40754fa3871dc
Em-tejano <mtejano.mt@gmail.com>
2025-02-25 12:17:33 +0800

    sync hash 1

--- server.js
+++ server.js
@@ -11,20 +11,23 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+/* bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
     //storing hash
     console.log(hash);
     bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
         //res = true/false
         console.log(res);
     });
-});
+}); */
 
 
 //END_ASYNC
 
 //START_SYNC
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 
 //END_SYNC
@@ -61,3 +64,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log("Listening on port:", PORT)
 });
+
+
+
